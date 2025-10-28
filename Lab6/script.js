3dc58e2236e28ad6e80912bc403256fba780ae85
lg2499 <lg2499@nau.edu>
2025-10-27 21:23:59 -0700

    Add files via upload

---
+++ Lab6/script.js
@@ -0,0 +1,6 @@
+function sortNumbers() {
+    const input = document.getElementById('numInput').value;
+    const numArray = input.split(',').map(Number);
+    numArray.sort((a, b) => a - b);
+    document.getElementById('output').textContent = numArray.join(', ');
+}
